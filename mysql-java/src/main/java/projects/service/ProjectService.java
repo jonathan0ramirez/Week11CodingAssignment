8bc4e68d35a7976da4c17ab1901512714343f591
jonathan0ramirez <jonathan_ramirez@techie.com>
2023-03-19 02:08:18 -0700

    week11Assignment

---
+++ mysql-java/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,48 @@
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+import projects.exception.DbException;
+
+public class ProjectService {
+
+	public Project addProject(Project project) {
+		
+		return projectDao.insertProject(project);
+	}
+	
+	private ProjectDao projectDao = new ProjectDao();
+
+	public List<Project> fetchAllProjects() {
+		
+		return projectDao.fetchAllProjects();
+	}
+
+	public Project fetchProjectById(Integer projectId) {
+		
+		return projectDao.fetchProjectById(projectId).orElseThrow(
+				() -> new NoSuchElementException(
+				"Project with project ID=" + projectId
+						+ "does not exist."));
+	}
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with Id="
+					+ project.getProjectId() + " does not exist.");
+		}
+		
+	}
+
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)){
+			throw new DbException("Project Id="
+					+ projectId + " does not exist.");
+		}
+		
+	}
+}
